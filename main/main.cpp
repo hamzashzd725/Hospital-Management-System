95bbd08c16952ab622ae4ea9ef4226f8e278a66f
hamzashzd725 <hamzashzd725@gmail.com>
2026-04-21 21:34:19 +0500

    Initial Commit: Adding main.cpp

---
+++ main/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+struct Patient {
+    int patientId;         // Patient ID
+    string name;           // Patient name
+    int age;
+    string gender;
+    string contact;
+    double balance;
+};
+Struct Doctor{
+    int doc_id;           // Doctor ID
+    string name;          // Doctor name
+    string specialty;     // e.g., Cardiologist, ENT
+    int experience;       // Years of experience
+};
+Struct Appointment
+struct Appointment {
+    int patientId;
+    int doctorId;
+    char date[11];        // e.g., "2026-03-30"
+    char time [10];        // e.g., "10:00 AM"
+};
+Struct Treatment
+struct Treatment {
+    int patientId;
+    string description; // Treatment details
+    double cost;        // Cost of treatment
+    bool paid;          // True if bill is paid
+};
+
+int main() {
+    char option == 'y';
+    while (option == 'y') {
+        int select;
+        cout<<"==========Welcome to hospital management system=========="<<endl;
+        cout<<"1. Patient"
+    }
+}
